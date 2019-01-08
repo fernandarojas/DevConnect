6ea44a6eef9645a455d677e20a604e8b5c27ec0d
Fernanda Rojas <FernandaRojas@fernandas-mbp.lan>
2019-01-07 22:03:36 -0500

    profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,15 @@
 const express = require('express'); 
 const router = express.Router(); 
+const mongoose = require('mongoose'); 
+const passport = require('passport'); 
 
 // @route   GET api/profile/test
 // @desc    Test profile route 
 // @access  Public
 router.get('/test', (req, res) => res.json({msg: "Profile Works"}));
+
+// @route   GET api/profile/current
+// @desc    Test profile route 
+// @access  Publi
 
 module.exports = router;
