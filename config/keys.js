319c815ab7c358ab19c7b4dc0cf371e476963a89
Fernanda Rojas <FernandaRojas@fernandas-mbp.lan>
2019-01-16 22:54:16 -0500

    redux

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = { 
-  mongoURI: 'mongodb://frojas:F331248@ds249824.mlab.com:49824/coursedev', 
+  mongoURI: 'mongodb://frojasdev:Eames331248@ds249824.mlab.com:49824/coursedev',
   secretOrKey: 'secret'
 }
